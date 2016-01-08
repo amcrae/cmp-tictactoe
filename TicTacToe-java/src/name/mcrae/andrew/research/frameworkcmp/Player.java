dc850a6b055c3e27b445e7196dc27dd2af422c59
andrewm <andrewm@andrews-mbp.lan>
2016-01-08 13:13:46 +1000

    + Intended behaviour description missing from previous commit.

--- TicTacToe-java/src/name/mcrae/andrew/research/frameworkcmp/Player.java
+++ TicTacToe-java/src/name/mcrae/andrew/research/frameworkcmp/Player.java
@@ -6,6 +6,9 @@ package name.mcrae.andrew.research.frameworkcmp;
  *  written for other games into this comparison framework, should one desire to ever do that. */
 public interface Player {
 
+	/** This name is required to be unique on the server. */
 	String getPlayerNamme();
+	
+	/** Inform the player that a move has been made so they can query the game state and act accordingly. */
 	void notifyMoveMade(Game g);
 }
