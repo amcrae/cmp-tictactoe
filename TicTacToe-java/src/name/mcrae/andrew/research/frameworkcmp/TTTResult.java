74729562d07665c783bd90dbf3c9f36c2e6525ae
andrewm <andrewm@andrews-mbp.lan>
2016-01-08 12:24:10 +1000

    + Add class to represent the outcome of a planned/actual move.

---
+++ TicTacToe-java/src/name/mcrae/andrew/research/frameworkcmp/TTTResult.java
@@ -0,0 +1,45 @@
+package name.mcrae.andrew.research.frameworkcmp;
+
+/** Represents the game state that results (or would result) 
+ * from a player making (or hypothetically attempting to make) a particular move in Tic-Tac-Toe.
+ * */
+public class TTTResult {
+
+	public enum MoveResult {
+		CONTINUE,
+		DRAW,
+		WIN
+	}
+	
+	private TTTMove lastMove;
+	private MoveResult moveResult;
+	private boolean permitted;
+	private TicTacToeBoard newState;
+	
+	public TTTMove getLastMove() {
+		return lastMove;
+	}
+	public void setLastMove(TTTMove lastMove) {
+		this.lastMove = lastMove;
+	}
+	public MoveResult getMoveResult() {
+		return moveResult;
+	}
+	public void setMoveResult(MoveResult moveResult) {
+		this.moveResult = moveResult;
+	}
+	public boolean isPermitted() {
+		return permitted;
+	}
+	public void setPermitted(boolean permitted) {
+		this.permitted = permitted;
+	}
+	public TicTacToeBoard getNewState() {
+		return newState;
+	}
+	public void setNewState(TicTacToeBoard newState) {
+		this.newState = newState;
+	}
+	
+	
+}
