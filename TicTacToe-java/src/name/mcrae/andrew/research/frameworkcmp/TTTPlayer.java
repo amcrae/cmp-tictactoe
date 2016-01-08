1e2f8ddcc51fbad4d1a7ecce5d9aab8de28c0d57
andrewm <andrewm@andrews-mbp.lan>
2016-01-08 13:17:42 +1000

    + More interface semantics.

--- TicTacToe-java/src/name/mcrae/andrew/research/frameworkcmp/TTTPlayer.java
+++ TicTacToe-java/src/name/mcrae/andrew/research/frameworkcmp/TTTPlayer.java
@@ -2,8 +2,12 @@ package name.mcrae.andrew.research.frameworkcmp;
 
 import name.mcrae.andrew.research.frameworkcmp.TicTacToeBoard.TTTPiece;
 
+/** This adds any messaging or implied state specific to a player of Tic-Tac-Toe. */
 public interface TTTPlayer extends Player {
 
+	/** As Tic-Tac-Toe is a board game with pieces placed by players, 
+	 * a player needs to know which piece has been assigned to them by the game system.
+	 * This leaves open the possibility that the player may first request to play a particular piece, which the Game may honour.
+	 * */
 	void setPiece(TTTPiece sym);
-	
 }
