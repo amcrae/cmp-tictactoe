e2ac6378e4b1e80a0caae22ccebd021b7783414f
Andrew McRae <ajmcrae@gmail.com>
2016-01-14 16:14:41 +1000

    Recognise a player may be playing multiple games.
    Therefore when they receive any message from the system they may need to
    know which game was the origin of the event.

--- TicTacToe-java/src/amcrae/research/frameworkcmp/TTTPlayer.java
+++ TicTacToe-java/src/amcrae/research/frameworkcmp/TTTPlayer.java
@@ -8,6 +8,8 @@ public interface TTTPlayer extends Player {
 	/** As Tic-Tac-Toe is a board game with pieces placed by players, 
 	 * a player needs to know which piece has been assigned to them by the game system.
 	 * This leaves open the possibility that the player may first request to play a particular piece, which the Game may honour.
+	 * @param game	The game in which the player has now been assigned a playing piece.
+	 * @param sym	The piece just assigned.
 	 * */
-	void setPiece(TTTPiece sym);
+	void setPiece(TicTacToeGame game, TTTPiece sym);
 }
